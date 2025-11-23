7aee6d9fb968bc24fe69c50b71e4ccabdaae3c22
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-11-23 12:50:27 +0100

    feat: create the method getAll for the CRUD

--- app/controllers/joke.controller.js
+++ app/controllers/joke.controller.js
@@ -1,2 +1,20 @@
 import { CoreController } from "./index.controller.js";
 import { Joke } from "../models/index.model.js";
+import { sequelize } from "../config.js/sequelize.client.js";
+
+// JokeController class to handle joke-related requests
+// Extends the CoreController to inherit common functionalities
+class JokeController extends CoreController {
+
+    // Method to get all jokes
+    getAll = async (req, res) => {
+        try {
+            const jokes = await Joke.findAll();
+            res.status(200).json(jokes);
+        } catch (error) {
+            this.json500(req, res, error);
+        }
+    }
+
+
+}
