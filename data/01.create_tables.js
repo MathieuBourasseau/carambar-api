07c8bc31c1ba3cc359d068f2d92416a8daffca1d
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-16 17:56:52 +0100

    feat: delete create table

--- data/01.create_tables.js
+++
@@ -1,8 +0,0 @@
-import { sequelize } from "../app/config/sequelize.client.js";
-import { Joke } from "../app/models/index.model.js"
-
-console.log("🚧 Tables creation");
-await sequelize.sync({ force: true });
-console.log("✅ Tables created successfully");
-
-await sequelize.close();
---
+++ init_db.js
@@ -0,0 +1,16 @@
+import { sequelize } from "./app/config/sequelize.client.js";
+
+async function initDB() {
+    try {
+        console.log("🚧 Synchronisation des tables...");
+        await sequelize.sync({ alter: true });
+
+        console.log("✅ Tables synchronisées avec succès !");
+        process.exit(0);
+    } catch (error) {
+        console.error("❌ Erreur lors de la création des tables :", error);
+        process.exit(1);
+    }
+}
+
+initDB();
